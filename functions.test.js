b525c0fe887e08cd3f17db972d7cb557511c6666
Matthew-Hallsworth-TfGM <matthew.hallsworth@tfgm.com>
2019-12-12 11:48:35 +0000

    updated

--- functions.test.js
+++ functions.test.js
@@ -53,7 +53,7 @@ test('User should be Brad Traversy', () => {
 });
 
 test('Should be under 1600', () => {
-  const load1 = 800;
+  const load1 = 500;
   const load2 = 700;
   expect(load1 + load2).toBeLessThan(1600);
 })
